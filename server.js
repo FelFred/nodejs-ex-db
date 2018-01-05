f82d086de7a20db3cad3668a6c5a5e3e624d539e
Felipe <felipe.fredes@ug.uchile.cl>
2018-01-05 16:06:39 -0300

    webhook test 4

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
     
 app.get('/', function(req, res){
-    res.send('hello ROOT world and webhook');
+    res.send('hello ROOT world and webhook 4');
 });
 
 console.log("Registering endpoint: /stubbed");
 app.get('/stubbed', function(req, res){
-    res.send('hello STUBBED 3');
+    res.send('hello STUBBED 4');
 });
 
 console.log("Registering endpoint: /testing");
