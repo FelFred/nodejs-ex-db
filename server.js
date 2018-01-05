6c62f9df82bb3397da70192d02dbe6a11932c9a5
Felipe <felipe.fredes@ug.uchile.cl>
2018-01-05 15:59:13 -0300

    webhook config v2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
     
 app.get('/', function(req, res){
-    res.send('hello ROOT world + webhook ');
+    res.send('hello ROOT world + webhook v2');
 });
 
 console.log("Registering endpoint: /stubbed");
