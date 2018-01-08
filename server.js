b339c7824ba55987ea18e4cbd4d792c63eed6762
Felipe <felipe.fredes@ug.uchile.cl>
2018-01-08 13:04:50 -0300

    test

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ var initDb = function(callback) {
 
 // endpoints
 app.get('/', function(req, res){
-    res.send('hello ROOT world, se añaden líneas para debug');
+    res.send('hello ROOT world. Se añaden líneas para debug');
     /*
    // try to initialize the db on every request if it's not already
   // initialized.
