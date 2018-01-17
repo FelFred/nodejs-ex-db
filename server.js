861040b8f6a19ed4c8c655fba1c07f485dd23161
Felipe Fredes <felipe.fredes@ug.uchile.cl>
2018-01-17 18:00:26 -0300

    Version 1.5: MariaDB adaptation. Try = 1

--- server.js
+++ server.js
@@ -18,8 +18,21 @@ Object.assign=require('object-assign');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+    mariaURL = process.env.OPENSHIFT_MARIADB_DB_URL || process.env.MARIA_URL,
+    mariaURLLabel = "";
+
+//Importar sql
+var mysql=require('mysql');
+
+//Crear variable de conexion
+var connection = mysql.createConnection( {
+	host : ip,
+	user: 'felipe',
+	password: 'practicazweicom',
+	database: 'users',
+	port: port
+});
+
 
 // FUNCTION: env_data -> Look at env through prints in console
 var env_data = function () {
@@ -28,58 +41,59 @@ var env_data = function () {
 	//console.log(process.env);
 	console.log("\nPort =" + port);
 	console.log("IP =" +  ip);
-	console.log("mongoURL =" + mongoURL);
+	console.log("mariaURL =" + mariaURL);
 	console.log("\n\nDATABASE_SERVICE_NAME =" + process.env.DATABASE_SERVICE_NAME);
-	console.log("\n\n Mongo data:\n")
-	console.log("mongoServiceName =" + mongoServiceName);
-	console.log("mongoHost =" + mongoHost);
-	console.log("mongoPort =" + mongoPort);
-	console.log("mongoDatabase =" + mongoDatabase);
-	console.log("mongoPassword =" + mongoPassword);
-	console.log("mongoUser =" + mongoUser);
-	console.log("\nmongoURLLabel =" + mongoURLLabel);
-	console.log("mongoURL =" + mongoURL);
+	console.log("\n\n Maria data:\n")
+	console.log("mariaServiceName =" + mariaServiceName);
+	console.log("mariaHost =" + mariaHost);
+	console.log("mariaPort =" + mariaPort);
+	console.log("mariaDatabase =" + mariaDatabase);
+	console.log("mariaPassword =" + mariaPassword);
+	console.log("mariaUser =" + mariaUser);
+	console.log("\nmariaURLLabel =" + mariaURLLabel);
+	console.log("mariaURL =" + mariaURL);
 
 }
 
 // Database info
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+if (mariaURL == null && process.env.DATABASE_SERVICE_NAME) {
 	
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
+  var mariaServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mariaHost = process.env[mariaServiceName + '_SERVICE_HOST'],
+      mariaPort = process.env[mariaServiceName + '_SERVICE_PORT'],
+      mariaDatabase = process.env[mariaServiceName + '_DATABASE'],
+      mariaPassword = process.env[mariaServiceName + '_PASSWORD']
+      mariaUser = process.env[mariaServiceName + '_USER'];
 
-  if (mongoHost && mongoPort && mongoDatabase) {
+  if (mariaHost && mariaPort && mariaDatabase) {
 
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    mariaURLLabel = mariaURL = 'mariadb://';
+    if (mariaUser && mariaPassword) {
+      mariaURL += mariaUser + ':' + mariaPassword + '@';
     }
     // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+    mariaURLLabel += mariaHost + ':' + mariaPort + '/' + mariaDatabase;
+    mariaURL += mariaHost + ':' +  mariaPort + '/' + mariaDatabase;
 
   }
 
 }
 
+/*
 var db = null,
     dbDetails = new Object();
 
 var initDb = function(callback) {
 
   console.log("Running initDb");	
-  if (mongoURL == null) {
-  	console.log("mongoURL = null");
+  if (mariaURL == null) {
+  	console.log("mariaURL = null");
   	return;
   }
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) {
-  	console.log("mongodb = null");
+  var mariadb = require('mariadb');
+  if (mariadb == null) {
+  	console.log("mariadb = null");
   	return;
   }
 
@@ -99,7 +113,7 @@ var initDb = function(callback) {
   });
 
 };
-
+*/
 
 // Endpoints
 app.get('/', function(req, res){
@@ -108,7 +122,7 @@ app.get('/', function(req, res){
 	client_external_ip = req.headers['x-forwarded-for'];
 	console.log("Received GET request to ROOT (/)");
 	console.log("Client's IP = " + client_ip);
-    res.send('Hello ROOT world. Bienvenido a la versión 1.4 de la app.\n \n Su dirección IP es: '+ client_external_ip); 
+    res.send('Hello ROOT world. Bienvenido a la versión 1.5 de la app.\n \n Su dirección IP es: '+ client_external_ip); 
 
 });
 
@@ -116,7 +130,7 @@ console.log("Registering endpoint: /version");
 app.get('/version', function(req, res){ 
 
 	console.log("Received GET request to /version");
-    res.send('Version: 1.4: IP check and registry');  // 1.0 =  1ra version modificada del día 09/01/2018    
+    res.send('Version: 1.5: MariaDB adaptation');  // 1.0 =  1ra version modificada del día 09/01/2018    
 
 });
 
@@ -166,54 +180,73 @@ console.log("Registering createcollection: /create");
 app.get('/create', function(req, res){
 
 	console.log("\nReceived GET request to /create");
-	console.log("Attempting to connect to mongodb...");
- 	mongodb.connect(mongoURL, function(err, db) {
-	    if (err) {
-	   	  console.log("Error al conectar con DB @ /create");
+
+	// Mariadb code
+	console.log("Attempting to connect to mariadb...");
+	con.connect(function(err) {
+	  	if (err) {
+  		  console.log("Error al conectar con DB @ /create");
 	   	  res.send("Error while attempting to connect to DB @ /create.");
 	   	  console.log(err);
-	      callback(err);
-	      return;
-	    }		 	
-	    db.createCollection("customers", function(op_error, result) {
-	    	if (op_error) {
-	    		console.log("Error found while attempting to create collection");
+	  	  //throw err;
+	  	}	
+	  	console.log("Connected!");
+	  	// Crea tabla "customers"
+	  	var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+	  	con.query(sql, function (op_error, result) {
+	    	if (op_error) { 
+	    		console.log("Error found while attempting to create table: customers");
 	    		console.log(op_error);
-	    		res.send("Error found while attempting to create collection");
+	    		res.send("Error found while attempting to create table: customers");
 	    		//throw err;
-	   		}
-	    	console.log("Collection created!");
-	    	db.close();
-	    });
-	    var myobj = [
-	    { _id: 1, name: 'John', address: 'Highway 71'},
-	    { _id: 2, name: 'Peter', address: 'Lowstreet 4'},
-	    { _id: 3, name: 'Amy', address: 'Apple st 652'},
-	    { _id: 4, name: 'Hannah', address: 'Mountain 21'},
-	    { _id: 5, name: 'Michael', address: 'Valley 345'},
-	    { _id: 6, name: 'Sandy', address: 'Ocean blvd 2'},
-	    { _id: 7, name: 'Betty', address: 'Green Grass 1'},
-	    { _id: 8, name: 'Richard', address: 'Sky st 331'},
-	    { _id: 9, name: 'Susan', address: 'One way 98'},
-	    { _id: 10, name: 'Vicky', address: 'Yellow Garden 2'},
-	    { _id: 11, name: 'Ben', address: 'Park Lane 38'},
-	    { _id: 12, name: 'William', address: 'Central st 954'},
-	    { _id: 13, name: 'Chuck', address: 'Main Road 989'},
-	    { _id: 14, name: 'Viola', address: 'Sideway 1633'}
-	  ];
-	  
-	  	db.collection("customers").insertMany(myobj, function(op_error, result) {
-	    	if (op_error) {
-	    		console.log("Error found while attempting to insert documents into the collection.")
-	    		console.log(op_error);
-	    		res.send({message:"Error found while attempting to insert documents into the collection.", error: op_error.message});
-	   			//throw err;
-	   		}
-	    	console.log("Number of documents inserted: " + res.insertedCount);
-	    	db.close();
+	    	}
+	    	console.log("Table 'customers' created");
 	  	});
+	  	// Crea tabla registry
+	  	var sql = "CREATE TABLE registry (id INT AUTO_INCREMENT PRIMARY KEY, ip VARCHAR(255), date VARCHAR(255))";
+	  	con.query(sql, function (op_error, result) {
+	    	if (op_error) { 
+	    		console.log("Error found while attempting to create table: registry");
+	    		console.log(op_error);
+	    		res.send("Error found while attempting to create table: registry");
+	    		//throw err;
+	    	}
+	    	console.log("Table 'customers' created");
+	  	});
+
+
+	  	var sql = "INSERT INTO customers (name, address) VALUES ?";
+	  	var values = [
+	    ['John', 'Highway 71'],
+	    ['Peter', 'Lowstreet 4'],
+	    ['Amy', 'Apple st 652'],
+	    ['Hannah', 'Mountain 21'],
+	    ['Michael', 'Valley 345'],
+	    ['Sandy', 'Ocean blvd 2'],
+	    ['Betty', 'Green Grass 1'],
+	    ['Richard', 'Sky st 331'],
+	    ['Susan', 'One way 98'],
+	    ['Vicky', 'Yellow Garden 2'],
+	    ['Ben', 'Park Lane 38'],
+	    ['William', 'Central st 954'],
+	    ['Chuck', 'Main Road 989'],
+	    ['Viola', 'Sideway 1633']
+	  	];
+
+	    con.query(sql, [values], function (op_error, result) {
+		    if (op_error) {
+		    	console.log("Error found while attempting to insert records into the table.")
+	    		console.log(op_error);
+	    		res.send({message:"Error found while attempting to insert records into the table.", error: op_error.message});
+		    // throw err;
+			}
+		    console.log("Number of records inserted: " + result.affectedRows);
+	  	});
+
 	});
-    
+
+
+	
 });
 
 
@@ -222,39 +255,38 @@ app.get('/data', function(req, res){
 
 	console.log("\nReceived GET request to /data");
 
-	console.log("Attempting to connect to mongodb...")    
-    mongodb.connect(mongoURL, function(err, db) {
+	// Mariadb code
+	console.log("Attempting to connect to mariadb...");
+	con.connect(function(err) {
+  		if (err) { 
+  			console.log("Error al conectar con DB @ data");	
+  			//throw err;
+  		}
+	  con.query("SELECT * FROM customers", function (op_error, result, fields) {
+	    if (op_error) {
+	    	console.log("Error found while attempting to get all data.");
+  			console.log(op_error);		  			
+  			res.send("Error found while attempting to get all data.");
+        	//throw err;
+	    }	    
+	    console.log(result);
+	    res.json(result);
+	    var sql = "INSERT INTO customers (name, address) VALUES ?";
+	    var values = [
+    	[client_external_ip, new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') ]
+    	];
+		con.query(sql, [values], function (op_error, result) {
+		    if (op_error) {
+	    		console.log("Error found while attempting to save registry.");
+  				console.log(op_error);		  			
+  				res.send("Error found while attempting to save registry");
+     			//throw err;
+		    }
+		    console.log("Date record inserted into registry");
+		});
 
-	    if (err) {
-	   	  console.log("Error al conectar con DB @ data")
-	      callback(err);
-	      return;
-	    }
-	    client_external_ip = req.headers['x-forwarded-for'];
-	    if (client_external_ip == "152.231.106.195" || client_external_ip == "191.126.173.182") {
-      	db.collection("customers").find({}).toArray(function(op_error, result) {
-	  		if (op_error) {
-	  			console.log("Error found while attempting to get all data.");
-	  			console.log(op_error);		  			
-	  			res.send("Error found while attempting to get all data.");
-	  			//throw err;
-	  		}
-
-	    	console.log(result); // entrega json en consola que corre el servidor
-	    	//res.json(JSON.stringify(result));  // entrega string del json encerrado por paréntesis cuadrádos [<json>], por ser un arreglo
-	    	res.json(result); // Entrega arreglo con resultados en la consola del cliente
-	    	//res.jsonp(result); //en este caso hace lo mismo que el anterior, falta leer documentación
-	    	var col = db.collection('registry');
-				// Create a document with request IP and current time of request
-			col.insert({ip: client_external_ip, date: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') });
-	    	db.close();
-	 	})
- 	 	} else {
- 	 		res.send("Not authorized (wrong IP)");
- 	 	}
-
- 	});
-    
+	  });
+	});
 
 });
 
@@ -271,27 +303,38 @@ app.post('/data', function(req, res){
 	console.log("Header data: " + req.headers); 
 
 
-	console.log("Attempting to connect to mongodb...");	
-	mongodb.connect(mongoURL, function(err, db) {
+	// Mariadb code
+	console.log("Attempting to connect to mariadb...");
+	con.connect(function(err) {
+  		if (err) { 
+  			console.log("Error al conectar con DB @ data (post)");	
+  			//throw err;
+  		}
+  		user_name = body_data.name;
+	  	con.query("SELECT * FROM customers WHERE name= ?",[user_name], function (op_error, result, fields) {
+	    if (op_error) {
+	    	console.log("Error found while attempting to get a particular record.");
+  			console.log(op_error);		  			
+  			res.send("Error found while attempting to get a particular record.");
+        	//throw err;
+	    }	    
+	    console.log(result);
+	    res.json(result);
+	    var sql = "INSERT INTO customers (name, address) VALUES ?";
+	    var values = [
+    	[client_external_ip, new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') ]
+    	];
+		con.query(sql, [values], function (op_error, result) {
+		    if (op_error) {
+	    		console.log("Error found while attempting to save registry.");
+  				console.log(op_error);		  			
+  				res.send("Error found while attempting to save registry");
+     			//throw err;
+		    }
+		    console.log("Date record inserted into registry");
+		});
 
-	    if (err) {
-	   	  console.log("Error al conectar con DB @ /data (POST)")
-	      callback(err);
-	      return;
-	    }		      
-	    db.collection("customers").findOne(body_data, function(op_error, result) {
-	    	if (op_error) {
-		 		console.log("Error found while attempting to get a particular document.");
-		 		console.log(op_error);
-	     		res.send("Error found while attempting to get a particular document.");
-	    		//throw err;
-	    	}
-	    	console.log("Trying to find document...")
-	    	console.log(result); //Se imprime output en consola del servidor
-	    	res.json(result);    //Se imprime output en consola del cliente
-	    	db.close();
-	    });
-
+	  });
 	});
     
 }); 
@@ -301,34 +344,25 @@ app.get('/registry', function(req, res){
 
 	console.log("\nReceived GET request to /registry");
 
-	console.log("Attempting to connect to mongodb...");
-	mongodb.connect(mongoURL, function(err, db) {
-
-	    if (err) {
-	   	  console.log("Error al conectar con DB @ registry")
-	      callback(err);
-	      return;
-	    }
-	    client_external_ip = req.headers['x-forwarded-for'];
-	    if (client_external_ip == "152.231.106.195" || client_external_ip == "191.126.173.182") {
-      	db.collection("registry").find({}).toArray(function(op_error, result) {
-	  		if (op_error) {
-	  			console.log("Error found while attempting to get all data.");
-	  			console.log(op_error);		  			
-	  			res.send("Error found while attempting to get all data.");
-	  			//throw err;
-	  		}
-	    	console.log(result); // entrega json en consola que corre el servidor
-	    	res.json(result); // Entrega arreglo con resultados en la consola del cliente (arreglo de documentos en formato json)
-	    	//res.json(JSON.stringify(result));  // entrega string del json encerrado por paréntesis cuadrádos [<json>], por ser un arreglo
-	    	db.close();
-	 	})
- 	 	} else {
- 	 		res.send("Not authorized (wrong IP)");
- 	 	}
-
- 	});
-    
+		// Mariadb code
+	console.log("Attempting to connect to mariadb...");
+	con.connect(function(err) {
+  		if (err) { 
+  			console.log("Error al conectar con DB @ data");	
+  			//throw err;
+  		}
+	  con.query("SELECT * FROM registry", function (op_error, result, fields) {
+	    if (op_error) {
+	    	console.log("Error found while attempting to get all data from registry.");
+  			console.log(op_error);		  			
+  			res.send("Error found while attempting to get all data from registry.");
+        	//throw err;
+	    }	    
+	    console.log(result);
+	    res.json(result);
+	    
+	  });
+	});
 
 });
 
